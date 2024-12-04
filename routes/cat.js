64f2c26eb3d22c5399ed7071449913f3ca2da102
仲尾次樹 <nakaoshiju@nakaoshijunoMacBook-Air.local>
2024-12-05 00:16:07 +0900

    add report api

--- routes/cat.js
+++ routes/cat.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 router.get('/', async (req, res) => {
-request('https://opentdb.com/api.php?amount=10', function (error, response,
+request('https://api.thecatapi.com/v1/images/search', function (error, response,
 body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
