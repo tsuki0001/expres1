746abb79a22ff747ba9d4a9a207cacf3efeaa6e5
仲尾次樹 <nakaoshiju@nakaoshijunoMacBook-Air.local>
2025-01-29 19:00:35 +0900

    add env

--- routes/note_from_b.js
+++ routes/note_from_b.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
+
+// corsミドルウェアを使用
+router.use(cors());
+
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
-// idが１のドキュメントを取得
-const query = { id: 2 };
-const note = await notes.findOne(query);
+
+
+// 全てのドキュメントを取得
+const note = await notes.find({}).toArray();
+
 res.json(note);
 })
+
 module.exports = router;
